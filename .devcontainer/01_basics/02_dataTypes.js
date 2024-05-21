8a651bd685e0e42a4bcef333f40a5593f1fde7cc
Afghan202 <127496843+Afghan202@users.noreply.github.com>
2024-05-21 08:15:07 +0000

    dataTypes

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -6,12 +6,10 @@ let name="Sami"
 let isAdmin=true // it use for true and false vlaue/ boolean type
 let address=null 
 let state; //undefined
-let mySymbol="%" //symbol dataType
 
 console.log(typeof 20);
 console.log(typeof isAdmin )
 console.log(typeof name);
 console.log(typeof address);
 console.log(typeof state)
-console.log(mySymbol);
 
