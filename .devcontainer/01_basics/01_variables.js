677a523407de4955706b5489f9406cb440354c89
Afghan202 <127496843+Afghan202@users.noreply.github.com>
2024-05-21 07:37:01 +0000

    differenct between var ,const and let

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=223333
+let accountEmail="hasib@gmail.com"
+var accountpassword="12222"
+let accountState;
+
+accountCity="Punjab"
+// accountEmail="hah@gmail.com"
+// accountpassword="22222"
+// accountId=4444
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState]);
+
+/*
+prefer not use var because of issue in block scope and functional scope
+*/
