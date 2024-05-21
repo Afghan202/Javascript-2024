c88a46b7ae262543e9b3faa3714557431d305eb9
Afghan202 <127496843+Afghan202@users.noreply.github.com>
2024-05-21 08:13:16 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+//dataTypes 
+let age=20 //number type can go up to 2 to power 53
+let name="Sami"
+let isAdmin=true // it use for true and false vlaue/ boolean type
+let address=null 
+let state; //undefined
+let mySymbol="%" //symbol dataType
+
+console.log(typeof 20);
+console.log(typeof isAdmin )
+console.log(typeof name);
+console.log(typeof address);
+console.log(typeof state)
+console.log(mySymbol);
+
