1e244ab5bae1ba27f12dfc49485af15a6eade2e3
Afghan202 <127496843+Afghan202@users.noreply.github.com>
2024-05-30 17:22:11 +0000

    operations, prefix and postfix

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score="Hitish"
 
-console.log(typeof (score))
+// console.log(typeof (score))
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 //"33" =>33
 //"33abc"=>NaN
@@ -13,8 +13,47 @@ console.log(valueInNumber)
 
 let isLoggedIn="anmol"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1=>true; 0=> false
 //"" =>false
 //"anmol"=>true
+
+// Operations
+let value=3
+let negValue=-value
+// console.log(-value)
+// console.log(2-2)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**3)
+
+// let str1="hello"
+// let str2="world"
+// let str3=str1 + str2
+// console.log(str3);
+
+// prefix and postfix
+
+// Expected output: "x:4, y:3"
+let x = 3;
+const y = x++;
+
+console.log(`x:${x},y:${y}`)
+
+// Expected output: "a:4, b:4"
+let a=3
+let b=++a
+console.log(`a:${a},:b${b}`);
+
+//expected output:"c:4,d:4"
+let c=4;
+let d=c++
+console.log(`c:${c},d:${d}`); 
+
+// expected out is t=1 and u=2
+let t=2;
+let u=t--;
+console.log(`t:${t},u:${u}`);
