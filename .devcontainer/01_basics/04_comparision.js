d1ca291744154e4c37ae0031961cd598c8e0665c
Afghan202 <127496843+Afghan202@users.noreply.github.com>
2024-05-30 17:37:14 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2>=1)
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//In comparision dataTypes must be same
+console.log("2">2)
+console.log("2">=2)
+console.log("2"<=2)
+console.log(null>0)
+
+//=== this check with the dataType
+console.log("2"===2)
