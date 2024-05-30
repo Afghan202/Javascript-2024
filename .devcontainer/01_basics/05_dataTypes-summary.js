2e94e22c04bdbe7d6576b5296ded148abf521b9f
Afghan202 <127496843+Afghan202@users.noreply.github.com>
2024-05-30 18:08:35 +0000

    summary of dataTypes

---
+++ .devcontainer/01_basics/05_dataTypes-summary.js
@@ -0,0 +1,34 @@
+//primitive 
+// 7 types string,boolean,bigInt,Number,null,undefined,symbol
+const score=100;
+const scoreValue=100.3
+const name="hi";
+const isLoggedIn=false;
+const outsideTemp=null;
+let userEmail;
+const id=Symbol("222");
+const anotherId=Symbol("222");
+const bigNumber=32223345888888748874788747487n
+
+// console.log( id===anotherId )
+
+
+
+
+
+//Non-primitive
+//object,array,functions
+const heros=["zahid","sami","hasib"]
+// console.log(heros)
+
+const myObj={
+    name:"Sami",
+    id:"123"
+};
+// console.log(myObj)
+
+const myFunction=function(){
+   console.log('clnak');
+}
+
+// console.log(typeof myFunction)
